c7dbf504e78e605bf513b82a75e10d43c28ff1d8
Josh Dassinger <dassjosh@users.noreply.github.com>
2020-12-26 09:57:51 -0600

    Some improvements to the snowflake converter

--- Oxide.Ext.Discord/Helpers/Converters/SnowflakeConverter.cs
+++ Oxide.Ext.Discord/Helpers/Converters/SnowflakeConverter.cs
@@ -6,19 +6,29 @@ namespace Oxide.Ext.Discord.Helpers.Converters
 {
     public class SnowflakeConverter : JsonConverter
     {
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            switch (reader.TokenType)
+            {
+                case JsonToken.Integer:
+                    return new Snowflake(ulong.Parse(reader.Value.ToString()));
+
+                case JsonToken.String:
+                    if (Snowflake.TryParse(reader.Value.ToString(), out Snowflake snowflake))
+                    {
+                        return snowflake;
+                    }
+
+                    throw new JsonException();
+                
+                default:
+                    throw new JsonException();
+            }
+        }
+        
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             writer.WriteValue(((Snowflake) value).ToString());
-        }
-
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            if (Snowflake.TryParse(reader.Value.ToString(), out Snowflake id))
-            {
-                return id;
-            }
-
-            return default(Snowflake);
         }
 
         public override bool CanConvert(Type objectType)
