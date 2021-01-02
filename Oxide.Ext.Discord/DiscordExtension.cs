ead96220af2a38999039bc3c9443d3c795f9b837
Josh Dassinger <dassjosh@users.noreply.github.com>
2021-01-02 07:41:42 -0600

    Remove Test Version Tag

--- Oxide.Ext.Discord/DiscordExtension.cs
+++ Oxide.Ext.Discord/DiscordExtension.cs
@@ -11,7 +11,7 @@ namespace Oxide.Ext.Discord
     public class DiscordExtension : Extension
     {
         private static readonly VersionNumber ExtensionVersion = new VersionNumber(1, 0, 7);
-        public const string TestVersion = "RC.2";
+        public const string TestVersion = "";
         
         private readonly ILogger _logger;
         
@@ -28,7 +28,7 @@ namespace Oxide.Ext.Discord
 
         public override VersionNumber Version => ExtensionVersion;
 
-        public static string GetExtensionVersion => ExtensionVersion + "." + TestVersion; 
+        public static string GetExtensionVersion => ExtensionVersion + TestVersion; 
 
         public override void OnModLoad()
         {
