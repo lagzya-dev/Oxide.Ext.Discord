2b1e761442eef9bc0a2ed872d1651bc326dcbd2a
Josh Dassinger <dassjosh@users.noreply.github.com>
2021-02-23 18:09:44 -0600

    Add some default xml comments to integration events until discord api docs are updated

--- Oxide.Ext.Discord/Entities/Gatway/Events/IntegrationCreate.cs
+++ Oxide.Ext.Discord/Entities/Gatway/Events/IntegrationCreate.cs
@@ -3,9 +3,15 @@ using Oxide.Ext.Discord.Entities.Integrations;
 
 namespace Oxide.Ext.Discord.Entities.Gatway.Events
 {
+    /// <summary>
+    /// Represents Integration Create Structure TODO:Add URL Once Published
+    /// </summary>
     [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
     public class IntegrationCreate : Integration
     {
+        /// <summary>
+        /// Guild Id the Integration was created in
+        /// </summary>
         [JsonProperty("guild_id")]
         public Snowflake GuildId { get; set; }
     }
--- Oxide.Ext.Discord/Entities/Gatway/Events/IntegrationUpdate.cs
+++ Oxide.Ext.Discord/Entities/Gatway/Events/IntegrationUpdate.cs
@@ -3,9 +3,15 @@ using Oxide.Ext.Discord.Entities.Integrations;
 
 namespace Oxide.Ext.Discord.Entities.Gatway.Events
 {
+    /// <summary>
+    /// Represents Integration Update Structure TODO:Add URL Once Published
+    /// </summary>
     [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
     public class IntegrationUpdate : Integration
     {
+        /// <summary>
+        /// Guild ID the integration was updated In
+        /// </summary>
         [JsonProperty("guild_id")]
         public Snowflake GuildId { get; set; }
     }
