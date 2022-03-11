93bfd4bc47d0416ee4cdcffe8aeb01a7bd10d076
Josh Dassinger <dassjosh@users.noreply.github.com>
2022-03-11 12:15:13 -0600

    Cleanup Enum & Localization

--- Oxide.Ext.Discord/Plugins/LangKeys.cs
+++
@@ -1,9 +0,0 @@
-namespace Oxide.Ext.Discord.Plugins
-{
-    internal static class LangKeys
-    {
-        public const string Chat = nameof(Chat);
-        public const string Version = nameof(Version);
-        public const string ReconnectWebSocket = nameof(ReconnectWebSocket);
-    }
-}
--- Oxide.Ext.Discord/Plugins/Localization.cs
+++ Oxide.Ext.Discord/Plugins/Localization.cs
@@ -1,6 +1,13 @@
 using System.Collections.Generic;
 namespace Oxide.Ext.Discord.Plugins
 {
+    internal static class LangKeys
+    {
+        public const string Chat = nameof(Chat);
+        public const string Version = nameof(Version);
+        public const string ReconnectWebSocket = nameof(ReconnectWebSocket);
+    }
+    
     internal static class Localization
     {
         internal static readonly Dictionary<string, Dictionary<string, string>> Languages = new Dictionary<string, Dictionary<string, string>>
