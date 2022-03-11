38af07ee4d3a0f8a4d6d75c64b4ebccb36c6e7ef
Josh Dassinger <dassjosh@users.noreply.github.com>
2022-03-11 15:00:42 -0600

    Updated application flag descriptions per: https://github.com/discord/discord-api-docs/commit/03d654008694bfbfcf6f43332ad6aa84c1f18e27

--- Oxide.Ext.Discord/Entities/Applications/ApplicationFlags.cs
+++ Oxide.Ext.Discord/Entities/Applications/ApplicationFlags.cs
@@ -14,42 +14,44 @@ namespace Oxide.Ext.Discord.Entities.Applications
         None = 0,
         
         /// <summary>
-        /// The application is verified and can use the GUILD_PRESENCES intent
+        /// Intent required for bots in 100 or more servers to receive presence_update events
         /// </summary>
         GatewayPresence = 1 << 12,
         
         /// <summary>
-        /// The application has the GUILD_PRESENCES intent enabled on the bot
+        /// Intent required for bots in under 100 servers to receive presence_update events, found in Bot Settings
         /// </summary>
         GatewayPresenceLimited = 1 << 13,
         
         /// <summary>
-        /// The application is verified and can use the GUILD_MEMBERS intent
+        /// Intent required for bots in 100 or more servers to receive member-related events like guild_member_add.
+        /// See list of member-related events under GUILD_MEMBERS
         /// </summary>
         GatewayGuildMembers = 1 << 14,
         
         /// <summary>
-        /// The application has the GUILD_MEMBERS intent enabled on the bot
+        /// Intent required for bots in under 100 servers to receive member-related events like guild_member_add, found in Bot Settings.
+        /// See list of member-related events under GUILD_MEMBERS
         /// </summary>
         GatewayGuildMembersLimited = 1 << 15,
         
         /// <summary>
-        /// The application is currently pending verification
+        /// Indicates unusual growth of an app that prevents verification
         /// </summary>
         VerificationPendingGuildLimit = 1 << 16,
         
         /// <summary>
-        /// The application has functionality that is specific to the discord client app.
+        /// Indicates if an app is embedded within the Discord client (currently unavailable publicly)
         /// </summary>
         Embedded = 1 << 17,
         
         /// <summary>
-        /// The application is verified and can use the Gateway Message intent
+        /// Intent required for bots in 100 or more servers to receive message content
         /// </summary>
         GatewayMessageContent = 1 << 18,
         
         /// <summary>
-        /// The application has the Gateway Message intent enabled on the bot
+        /// Intent required for bots in under 100 servers to receive message content, found in Bot Settings
         /// </summary>
         GatewayMessageContentLimited = 1 << 19,
     }
