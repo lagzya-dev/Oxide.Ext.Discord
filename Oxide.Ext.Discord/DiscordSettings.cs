8b5142da0339c1fd7ebe90cb7dffd0bce4b1765b
Josh Dassinger <dassjosh@users.noreply.github.com>
2022-03-20 17:24:16 -0500

    Improve GetHiddenToken to return only the part after the 2nd period.

--- Oxide.Ext.Discord/DiscordSettings.cs
+++ Oxide.Ext.Discord/DiscordSettings.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text;
 using Oxide.Ext.Discord.Entities.Gatway;
 using Oxide.Ext.Discord.Logging;
 
@@ -25,8 +25,8 @@ namespace Oxide.Ext.Discord
         /// See <see cref="GatewayIntents"/>
         /// </summary>
         public GatewayIntents Intents = GatewayIntents.None;
-
-        private static readonly Regex HideTokenRegex = new Regex("\\w|-", RegexOptions.Compiled);
+        
+        private string _hiddenToken;
         
         /// <summary>
         /// Hides the token but keeps the format to allow for debugging token issues without showing the token.
@@ -34,7 +34,23 @@ namespace Oxide.Ext.Discord
         /// <returns></returns>
         public string GetHiddenToken()
         {
-            return $"\"{HideTokenRegex.Replace(ApiToken, "#")}\"";
+            if (_hiddenToken != null)
+            {
+                return _hiddenToken;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            int first = ApiToken.IndexOf('.');
+            int second = ApiToken.IndexOf('.', first + 1);
+
+            sb.Append('#', first);
+            sb.Append('.');
+            sb.Append('#', second - first);
+            sb.Append('.');
+            sb.Append(ApiToken.Substring(second + 1));
+
+            _hiddenToken = sb.ToString();
+            return _hiddenToken;
         }
     }
 }
