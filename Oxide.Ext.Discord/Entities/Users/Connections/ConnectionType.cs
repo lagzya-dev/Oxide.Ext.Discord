ac6b8cfd715b826810a4b28caa4df4296a2502a1
Josh Dassinger <dassjosh@users.noreply.github.com>
2022-07-20 09:08:47 -0500

    Updated User Connection Types Per: https://github.com/discord/discord-api-docs/commit/40d728f5c30329753f6d1a653b274c5c07a04ebe

--- Oxide.Ext.Discord/Entities/Users/Connections/ConnectionType.cs
+++ Oxide.Ext.Discord/Entities/Users/Connections/ConnectionType.cs
@@ -16,9 +16,74 @@ namespace Oxide.Ext.Discord.Entities.Users.Connections
         Unknown,
         
         /// <summary>
+        /// Connection type is Battle.net
+        /// </summary>
+        [Description("battlenet")] BattleNet,
+        
+        /// <summary>
+        /// Connection type is Epic Games
+        /// </summary>
+        [Description("epicgames")] EpicGames,
+        
+        /// <summary>
+        /// Connection type is Facebook
+        /// </summary>
+        [Description("EpicGames")] Facebook,
+        
+        /// <summary>
+        /// Connection type is Youtube
+        /// </summary>
+        [Description("github")] GitHub,
+        
+        /// <summary>
+        /// Connection type is League of Legends
+        /// </summary>
+        [Description("leagueoflegends")] LeagueOfLegends,
+        
+        /// <summary>
+        /// Connection type is PlayStation Network
+        /// </summary>
+        [Description("playstation")] PlayStationNetwork,
+        
+        /// <summary>
+        /// Connection type is Reddit
+        /// </summary>
+        [Description("reddit")] Reddit,
+        
+        /// <summary>
+        /// Connection type is Samsung Galaxy
+        /// </summary>
+        [Description("samsunggalaxy")] SamsungGalaxy,   
+        
+        /// <summary>
+        /// Connection type is Spotify
+        /// </summary>
+        [Description("spotify")] Spotify,    
+        
+        /// <summary>
+        /// Connection type is Skype
+        /// </summary>
+        [Description("skype")] Skype,     
+        
+        /// <summary>
+        /// Connection type is Steam
+        /// </summary>
+        [Description("steam")] Steam, 
+        
+        /// <summary>
         /// Connection type is Twitch
         /// </summary>
-        [Description("twitch")] Twitch,
+        [Description("twitch")] Twitch,      
+        
+        /// <summary>
+        /// Connection type is Twitter
+        /// </summary>
+        [Description("twitter")] Twitter,   
+        
+        /// <summary>
+        /// Connection type is Xbox
+        /// </summary>
+        [Description("xbox")] Xbox,        
         
         /// <summary>
         /// Connection type is Youtube
