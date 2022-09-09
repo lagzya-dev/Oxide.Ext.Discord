9f1128d1bdd80425b1b171ee3291d88bb8f4bf5b
Josh Dassinger <dassjosh@users.noreply.github.com>
2022-09-09 11:01:26 -0500

    Missed placeholder

--- Oxide.Ext.Discord/Libraries/Placeholders/Default/MemberPlaceholders.cs
+++ Oxide.Ext.Discord/Libraries/Placeholders/Default/MemberPlaceholders.cs
@@ -1,19 +1,27 @@
 using System.Text;
+using Oxide.Core.Plugins;
 using Oxide.Ext.Discord.Entities.Guilds;
+using Oxide.Ext.Discord.Plugins.Core;
 
 namespace Oxide.Ext.Discord.Libraries.Placeholders.Default
 {
-    internal static class MemberPlaceholders
+    public static class MemberPlaceholders
     {
-        private static void Id(StringBuilder builder, PlaceholderState state, GuildMember member) => PlaceholderFormatting.Replace(builder, state, member.Id);
-        private static void Name(StringBuilder builder, PlaceholderState state, GuildMember member) => PlaceholderFormatting.Replace(builder, state, member.DisplayName);
-        private static void Mention(StringBuilder builder, PlaceholderState state, GuildMember member) => PlaceholderFormatting.Replace(builder, state, member.User.Mention);
+        public static void Id(StringBuilder builder, PlaceholderState state, GuildMember member) => PlaceholderFormatting.Replace(builder, state, member.Id);
+        public static void Name(StringBuilder builder, PlaceholderState state, GuildMember member) => PlaceholderFormatting.Replace(builder, state, member.DisplayName);
+        public static void Mention(StringBuilder builder, PlaceholderState state, GuildMember member) => PlaceholderFormatting.Replace(builder, state, member.User.Mention);
 
-        public static void RegisterPlaceholders(DiscordPlaceholders placeholders)
+        internal static void RegisterPlaceholders()
         {
-            placeholders.RegisterInternalPlaceholder<GuildMember>("member.id", Id);
-            placeholders.RegisterInternalPlaceholder<GuildMember>("member.name", Name);
-            placeholders.RegisterInternalPlaceholder<GuildMember>("member.mention", Mention);
+            RegisterPlaceholders(DiscordExtensionCore.Instance, "member", nameof(GuildMember));
+        }
+        
+        public static void RegisterPlaceholders(Plugin plugin, string placeholderPrefix, string dataKey)
+        {
+            DiscordPlaceholders placeholders = DiscordExtension.DiscordPlaceholders;
+            placeholders.RegisterPlaceholder<GuildMember>(plugin, $"{placeholderPrefix}.id", dataKey, Id);
+            placeholders.RegisterPlaceholder<GuildMember>(plugin, $"{placeholderPrefix}.name", dataKey, Name);
+            placeholders.RegisterPlaceholder<GuildMember>(plugin, $"{placeholderPrefix}.mention", dataKey, Mention);
         }
     }
 }
