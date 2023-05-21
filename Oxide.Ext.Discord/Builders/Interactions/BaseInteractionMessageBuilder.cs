0eaf2736f667104d1a679354dd62fb442c4a34ac
Josh Dassinger <dassjosh@users.noreply.github.com>
2023-05-21 14:34:13 -0500

    Cleanup BaseInteractionMessageBuilder

--- Oxide.Ext.Discord/Builders/Interactions/BaseInteractionMessageBuilder.cs
+++ Oxide.Ext.Discord/Builders/Interactions/BaseInteractionMessageBuilder.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
 using Oxide.Ext.Discord.Builders.Messages.BaseBuilders;
 using Oxide.Ext.Discord.Entities.Interactions;
-using Oxide.Ext.Discord.Entities.Interactions.MessageComponents;
 using Oxide.Ext.Discord.Entities.Interactions.Response;
 using Oxide.Ext.Discord.Entities.Messages;
-using Oxide.Ext.Discord.Entities.Messages.AllowedMentions;
-using Oxide.Ext.Discord.Entities.Messages.Embeds;
 using Oxide.Ext.Discord.Exceptions.Builders;
 
 namespace Oxide.Ext.Discord.Builders.Interactions
@@ -21,18 +16,13 @@ namespace Oxide.Ext.Discord.Builders.Interactions
         where TBuilder : BaseInteractionMessageBuilder<TMessage, TBuilder>
     {
         /// <summary>
-        /// Interaction this interaction builder is for
-        /// </summary>
-        protected readonly DiscordInteraction Interaction;
-
-        /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="interaction">Interaction this builder is for</param>
         /// <param name="message">Message to be created</param>
-        public BaseInteractionMessageBuilder(DiscordInteraction interaction, TMessage message) : base(message)
+        protected BaseInteractionMessageBuilder(DiscordInteraction interaction, TMessage message) : base(message)
         {
-            Interaction = interaction ?? throw new ArgumentNullException(nameof(interaction));
+            InteractionResponseBuilderException.ThrowIfInteractionIsAutoComplete(interaction.Type);
         }
         
         /// <summary>
@@ -41,74 +31,8 @@ namespace Oxide.Ext.Discord.Builders.Interactions
         /// <returns></returns>
         public TBuilder AsEphemeral()
         {
-            InteractionResponseBuilderException.ThrowIfInteractionIsAutoComplete(Interaction.Type);
             Message.Flags |= MessageFlags.Ephemeral;
             return Builder;
         }
-        
-        ///<inheritdoc/>
-        public override TBuilder SuppressEmbeds()
-        {
-            InteractionResponseBuilderException.ThrowIfInteractionIsAutoComplete(Interaction.Type);
-            return base.SuppressEmbeds();
-        }
-
-        #region Overrides
-        ///<inheritdoc/>
-        public override TBuilder AsTts(bool enabled = true)
-        {
-            InteractionResponseBuilderException.ThrowIfInteractionIsAutoComplete(Interaction.Type);
-            return base.AsTts(enabled);
-        }
-
-        ///<inheritdoc/>
-        public override TBuilder AddContent(string content)
-        {
-            InteractionResponseBuilderException.ThrowIfInteractionIsAutoComplete(Interaction.Type);
-            return base.AddContent(content);
-        }
-
-        ///<inheritdoc/>
-        public override TBuilder AddEmbed(DiscordEmbed embed)
-        {
-            InteractionResponseBuilderException.ThrowIfInteractionIsAutoComplete(Interaction.Type);
-            return base.AddEmbed(embed);
-        }
-
-        ///<inheritdoc/>
-        public override TBuilder AddEmbeds(ICollection<DiscordEmbed> embeds)
-        {
-            InteractionResponseBuilderException.ThrowIfInteractionIsAutoComplete(Interaction.Type);
-            return base.AddEmbeds(embeds);
-        }
-
-        ///<inheritdoc/>
-        public override TBuilder AddAllowedMentions(AllowedMention mention)
-        {
-            InteractionResponseBuilderException.ThrowIfInteractionIsAutoComplete(Interaction.Type);
-            return base.AddAllowedMentions(mention);
-        }
-
-        ///<inheritdoc/>
-        public override TBuilder AddActionRow(ActionRowComponent component)
-        {
-            InteractionResponseBuilderException.ThrowIfInteractionIsAutoComplete(Interaction.Type);
-            return Builder.AddActionRow(component);
-        }
-
-        ///<inheritdoc/>
-        public override TBuilder AddComponents(ICollection<ActionRowComponent> components)
-        {
-            InteractionResponseBuilderException.ThrowIfInteractionIsAutoComplete(Interaction.Type);
-            return base.AddComponents(components);
-        }
-
-        ///<inheritdoc/>
-        public override TBuilder AddAttachment(string filename, byte[] data, string contentType, string description = null)
-        {
-            InteractionResponseBuilderException.ThrowIfInteractionIsAutoComplete(Interaction.Type);
-            return base.AddAttachment(filename, data, contentType, description);
-        }
-        #endregion
     }
 }
