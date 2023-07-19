9273f0c97da6f30c4b9fe04f64893fd5258ee5b1
Josh Dassinger <dassjosh@users.noreply.github.com>
2023-07-19 08:21:43 -0500

    Update Templates to use new format

--- Oxide.Ext.Discord/Plugins/Core/DiscordExtensionCore.Templates.cs
+++ Oxide.Ext.Discord/Plugins/Core/DiscordExtensionCore.Templates.cs
@@ -1,9 +1,12 @@
 using System.Collections.Generic;
 using Oxide.Ext.Discord.Clients;
+using Oxide.Ext.Discord.Entities;
 using Oxide.Ext.Discord.Entities.Interactions;
 using Oxide.Ext.Discord.Entities.Interactions.Response;
 using Oxide.Ext.Discord.Entities.Messages;
+using Oxide.Ext.Discord.Extensions;
 using Oxide.Ext.Discord.Libraries.Placeholders;
+using Oxide.Ext.Discord.Libraries.Placeholders.Keys;
 using Oxide.Ext.Discord.Libraries.Templates;
 using Oxide.Ext.Discord.Libraries.Templates.Embeds;
 using Oxide.Ext.Discord.Libraries.Templates.Messages;
@@ -13,25 +16,22 @@ namespace Oxide.Ext.Discord.Plugins.Core
 {
     internal partial class DiscordExtensionCore
     {
-        private const string DiscordSuccess = "43b581";
-        private const string DiscordDanger = "f04747";
-        
         public void CreateTemplates()
         {
-            DiscordMessageTemplate success = CreateTemplateEmbed("You have successfully deleted application command {command.name}", DiscordSuccess);
+            DiscordMessageTemplate success = CreateTemplateEmbed($"You have successfully deleted application command {DefaultKeys.AppCommand.Name}", DiscordColor.Success);
             DiscordExtension.DiscordMessageTemplates.RegisterLocalizedTemplateAsync(this, TemplateKeys.Commands.Delete.Success, success, new TemplateVersion(1, 0, 0), new TemplateVersion(1, 0, 0));
             
-            DiscordMessageTemplate deleteError = CreateTemplateEmbed("An error occured deleting command {command.name}. Please try the command again. \nError: {error.message}", DiscordDanger);
+            DiscordMessageTemplate deleteError = CreateTemplateEmbed($"An error occured deleting command {DefaultKeys.AppCommand.Name}. Please try the command again. \nError: {DefaultKeys.ResponseError.Message}", DiscordColor.Danger);
             DiscordExtension.DiscordMessageTemplates.RegisterLocalizedTemplateAsync(this, TemplateKeys.Commands.Delete.Errors.DeleteCommandError, deleteError, new TemplateVersion(1, 0, 0), new TemplateVersion(1, 0, 0));
             
-            DiscordMessageTemplate commandNotFound = CreateTemplateEmbed("We failed to find a matching command. Please make sure you select a command from the drop down.", DiscordDanger);
+            DiscordMessageTemplate commandNotFound = CreateTemplateEmbed("We failed to find a matching command. Please make sure you select a command from the drop down.", DiscordColor.Danger);
             DiscordExtension.DiscordMessageTemplates.RegisterLocalizedTemplateAsync(this, TemplateKeys.Commands.Delete.Errors.CommandIdNotFound, commandNotFound, new TemplateVersion(1, 0, 0), new TemplateVersion(1, 0, 0));
             
-            DiscordMessageTemplate invalidSelection = CreateTemplateEmbed("You have selected an invalid command. Please try the command again and be sure to select an option from the drop down.", DiscordDanger);
+            DiscordMessageTemplate invalidSelection = CreateTemplateEmbed("You have selected an invalid command. Please try the command again and be sure to select an option from the drop down.", DiscordColor.Danger);
             DiscordExtension.DiscordMessageTemplates.RegisterLocalizedTemplateAsync(this, TemplateKeys.Commands.Delete.Errors.InvalidSelection, invalidSelection, new TemplateVersion(1, 0, 0), new TemplateVersion(1, 0, 0));
         }
         
-        public DiscordMessageTemplate CreateTemplateEmbed(string description, string color)
+        public DiscordMessageTemplate CreateTemplateEmbed(string description, DiscordColor color)
         {
             return new DiscordMessageTemplate
             {
@@ -40,7 +40,7 @@ namespace Oxide.Ext.Discord.Plugins.Core
                     new DiscordEmbedTemplate
                     {
                         Description = description,
-                        Color = $"#{color}"
+                        Color = color.ToHex()
                     }
                 },
             };
