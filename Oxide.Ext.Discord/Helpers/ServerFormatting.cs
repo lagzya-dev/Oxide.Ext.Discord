c3407cd946b25872fd97e0724e29f37294082819
Josh Dassinger <dassjosh@users.noreply.github.com>
2023-07-22 07:06:58 -0500

    Added Server Text Formatting helper methods

---
+++ Oxide.Ext.Discord/Helpers/ServerFormatting.cs
@@ -0,0 +1,12 @@
+﻿using Oxide.Ext.Discord.Entities;
+using Oxide.Ext.Discord.Extensions;
+using Oxide.Ext.Discord.Libraries.Placeholders;
+
+namespace Oxide.Ext.Discord.Helpers
+{
+    public static class ServerFormatting
+    {
+        public static string Color(string text, DiscordColor color) => $"[{color.ToHex()}]{text}[/#]";
+        public static string Color(this PlaceholderKey key, DiscordColor color) => $"[{color.ToHex()}]{key.ToString()}[/#]";
+    }
+}
