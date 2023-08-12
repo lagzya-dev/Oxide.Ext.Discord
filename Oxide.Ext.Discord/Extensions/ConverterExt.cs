1fa0d207b7c45af8829f10bcc5aa7fb2f452e610
Josh Dassinger <dassjosh@users.noreply.github.com>
2023-08-12 07:32:42 -0500

    Removed ConverterExt.cs

--- Oxide.Ext.Discord/Extensions/ConverterExt.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Oxide.Ext.Discord.Extensions
-{
-    internal static class ConverterExt
-    {
-        public static TTarget Convert<TTarget, TSource>(TSource entity) where TTarget : TSource => (TTarget)entity;
-    }
-}
