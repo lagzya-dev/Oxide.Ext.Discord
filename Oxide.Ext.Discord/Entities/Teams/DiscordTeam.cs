c2d7ca544beb3ce35851a4a10b36bf0d2479e053
Josh Dassinger <dassjosh@users.noreply.github.com>
2023-10-19 08:15:54 -0500

    Add Team Role per: https://github.com/discord/discord-api-docs/commit/80b0958e70f443dd8b145ec4b51a0eb04013b83a

--- Oxide.Ext.Discord/Entities/Teams/DiscordTeam.cs
+++ Oxide.Ext.Discord/Entities/Teams/DiscordTeam.cs
@@ -11,35 +11,41 @@ namespace Oxide.Ext.Discord.Entities.Teams
     public class DiscordTeam
     {
         /// <summary>
-        /// The unique id of the team
+        /// Unique ID of the team
         /// </summary>
         [JsonProperty("id")]
         public Snowflake Id { get; set; }
         
         /// <summary>
-        /// A hash of the image of the team's icon
+        /// Hash of the image of the team's icon
         /// </summary>
         [JsonProperty("icon")]
         public string Icon { get; set; }
         
         /// <summary>
-        /// The members of the team
+        /// Members of the team
         /// See <see cref="TeamMember"/>
         /// </summary>
         [JsonProperty("members")]
         public List<TeamMember> Members { get; set; }
         
         /// <summary>
-        ///  The name of the team
+        /// Name of the team
         /// </summary>
         [JsonProperty("name")]
         public string Name { get; set; }
         
         /// <summary>
-        /// The user id of the current team owner
+        /// User ID of the current team owner
         /// </summary>
         [JsonProperty("owner_user_id")]
         public Snowflake OwnerUserId { get; set; }
+        
+        /// <summary>
+        /// Role of the team member
+        /// </summary>
+        [JsonProperty("role")]
+        public TeamRole Role { get; set; }
 
         /// <summary>
         /// Returns the url for the team icon
