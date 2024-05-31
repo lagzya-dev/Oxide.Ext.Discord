59093aafd178730e989f261220e8e7b09107a5d5
dassjosh <joshman2934@gmail.com>
2024-05-31 11:46:01 -0500

    Add AutoMod Blocking Profile per: https://github.com/discord/discord-api-docs/commit/60a711d841394492fc3a36c8d14c19fea00c2ccd

--- Oxide.Ext.Discord/Entities/AutoMod/AutoModEventType.cs
+++ Oxide.Ext.Discord/Entities/AutoMod/AutoModEventType.cs
@@ -8,6 +8,11 @@ namespace Oxide.Ext.Discord.Entities
         /// <summary>
         /// When a member sends or edits a message in the guild
         /// </summary>
-        MessageSend = 1
+        MessageSend = 1,
+        
+        /// <summary>
+        /// When a member edits their profile
+        /// </summary>
+        MemberUpdate = 1
     }
 }
