d18760df8242877add6ec9ffe48bdb7b82affe80
dassjosh <joshman2934@gmail.com>
2024-05-31 11:23:18 -0500

    Add InviteType per: https://github.com/discord/discord-api-docs/commit/4c6b571f3c0b7dcb1a6da13b48a0714aebdb0ee2

---
+++ Oxide.Ext.Discord/Entities/Invites/InviteType.cs
@@ -0,0 +1,22 @@
+﻿namespace Oxide.Ext.Discord.Entities
+{    /// <summary>
+    /// Represents <a href="https://discord.com/developers/docs/resources/invite#invite-types">Invite Types</a>
+    /// </summary>
+    public enum InviteType : byte
+    {
+        /// <summary>
+        /// Guild Invite
+        /// </summary>
+        Guild = 0,
+        
+        /// <summary>
+        /// Guild Invite
+        /// </summary>
+        GroupDm = 1,
+        
+        /// <summary>
+        /// Guild Invite
+        /// </summary>
+        Friend = 2,
+    }
+}
